c81adb45e57a942ea383ff0d70fffa7854c1eb42
jelliotartz <jelliotartz@gmail.com>
2016-03-29 10:33:12 -0500

    add one-way and two-way data binding examples

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -1,12 +1,28 @@
+// new
 import {Component} from 'angular2/core';
 
 @Component({
-    selector: 'my-app',
-    template: `
-        <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
-    `,
+  selector: 'my-app',
+  template: `
+      <h3 (click)="onSelect()" [class.clicked]="showDetail === true">
+	      Hello, {{contact.firstName}} {{contact.lastName}}
+      </h3>
+      <input [(ngModel)]="contact.firstName" type="text" />
+      <div *ngIf ="showDetail === true">
+      	Phone Number: {{contact.phone}}<br>
+      	Email: {{contact.email}}
+      </div>
+      <p>This app cray!!</p>
+  `,
+  styleUrls: ["../src/css/app.css"]
 })
-export class AppComponent {
 
+export class AppComponent {
+  public contact = { firstName: "James", lastName: "Artz", phone: "123456789", email: "james@james.james" };
+
+  public showDetail = false;
+
+  onSelect() {
+    this.showDetail = true;
+  }
 }
