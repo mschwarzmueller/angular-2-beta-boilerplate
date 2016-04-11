72a51e56059d59b019ce6277b64f3d80173c168b
Maximilian Schwarzmüller <mblacky0@gmail.com>
2016-04-11 15:09:56 +0200

    Updated to beta.14, fixed duplicate identifiers bug

--- dev/boot.ts
+++ dev/boot.ts
@@ -1,4 +1,4 @@
-/// <reference path="../typings/browser.d.ts" />
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 import {bootstrap} from 'angular2/platform/browser';
 import {AppComponent} from "./app.component";
 
